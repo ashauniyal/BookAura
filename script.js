ea5fd2b1713a0230e377773e38875223ba3acfef
ashauniyal <ashauniyal1606@gmail.com>
2025-10-06 09:42:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-         // Toggle Dark/Light Mode
+        // Toggle Dark/Light Mode
         const modeToggle = document.getElementById('modeToggle');
         const body = document.body;
         const icon = modeToggle.querySelector('i');
@@ -34,24 +34,7 @@
             });
         });
 
-        // // Image Carousel Functionality
-        // const carouselSlides = document.querySelectorAll('.carousel-slide');
-        // let currentSlide = 0;
-
-        // function showSlide(n) {
-        //     carouselSlides.forEach(slide => slide.classList.remove('active'));
-        //     currentSlide = (n + carouselSlides.length) % carouselSlides.length;
-        //     carouselSlides[currentSlide].classList.add('active');
-        // }
-
-        // function nextSlide() {
-        //     showSlide(currentSlide + 1);
-        // }
-
-        // // Auto-advance carousel every 5 seconds
-        // setInterval(nextSlide, 5000);
-
-        // Login/Signup Dropdown Functionality
+        // // Login/Signup Dropdown Functionality
         // const accountBtn = document.getElementById('accountBtn');
         // const authDropdown = document.getElementById('authDropdown');
         // const showSignup = document.getElementById('showSignup');
@@ -60,6 +43,7 @@
         // const signupForm = document.getElementById('signupForm');
         // const loginBtn = document.getElementById('loginBtn');
         // const signupBtn = document.getElementById('signupBtn');
+       
 
         // // Toggle dropdown visibility
         // accountBtn.addEventListener('click', function(e) {
@@ -115,7 +99,6 @@
         //         alert('Please fill in all fields');
         //     }
         // });
-
          // Login/Signup Dropdown Functionality
       const accountBtn = document.getElementById("accountBtn");
       const authDropdown = document.getElementById("authDropdown");
@@ -585,142 +568,8 @@
         // Event listeners for voice controls
         playBtn.addEventListener('click', playSpeech);
         pauseBtn.addEventListener('click', pauseSpeech);
-        stopBtn.addEventListener('click', stopSpeech);
-
-      
-        
-
-    /* --- Books Data --- */
-    const allBooks = Array.from({length: 500}, (_, i) => ({
-      title: `Book Title ${i+1}`,
-      img: `https://picsum.photos/200/300?random=${i+1}`
-    }));
-
-    const bookGrid = document.getElementById("bookGrid");
-    const searchInput = document.getElementById("searchInput");
-
-    function displayBooks(books) {
-      bookGrid.innerHTML = books.map(book => `
-        <div class="book-card">
-          <img src="${book.img}" alt="${book.title}">
-          <h3>${book.title}</h3>
-          <button>Download</button>
-        </div>
-      `).join("");
-    }
-
-    $(document).ready(function() {
-  var item, tile, author, publisher, bookLink, bookImg;
-  var outputList = document.getElementById("list-output");
-  var bookUrl = "https://www.googleapis.com/books/v1/volumes?q=";
-  var apiKey = "key=AIzaSyDtXC7kb6a7xKJdm_Le6_BYoY5biz6s8Lw";
-  var placeHldr = '<img src="https://via.placeholder.com/150">';
-  var searchData;
-
-  //listener for search button
-  $("#search").click(function() {
-    outputList.innerHTML = ""; //empty html output
-    document.body.style.backgroundImage = "url('')";
-     searchData = $("#search-box").val();
-     //handling empty search input field
-     if(searchData === "" || searchData === null) {
-       displayError();
-     }
-    else {
-       // console.log(searchData);
-       // $.get("https://www.googleapis.com/books/v1/volumes?q="+searchData, getBookData()});
-       $.ajax({
-          url: bookUrl + searchData,
-          dataType: "json",
-          success: function(response) {
-            console.log(response)
-            if (response.totalItems === 0) {
-              alert("no result!.. try again")
-            }
-            else {
-              $("#title").animate({'margin-top': '5px'}, 1000); //search box animation
-              $(".book-list").css("visibility", "visible");
-              displayResults(response);
-            }
-          },
-          error: function () {
-            alert("Something went wrong.. <br>"+"Try again!");
-          }
-        });
-      }
-      $("#search-box").val(""); //clearn search box
-   });
-
-   /*
-   * function to display result in index.html
-   * @param response
-   */
-   function displayResults(response) {
-      for (var i = 0; i < response.items.length; i+=2) {
-        item = response.items[i];
-        title1 = item.volumeInfo.title;
-        author1 = item.volumeInfo.authors;
-        publisher1 = item.volumeInfo.publisher;
-        bookLink1 = item.volumeInfo.previewLink;
-        bookIsbn = item.volumeInfo.industryIdentifiers[1].identifier
-        bookImg1 = (item.volumeInfo.imageLinks) ? item.volumeInfo.imageLinks.thumbnail : placeHldr ;
-
-        item2 = response.items[i+1];
-        title2 = item2.volumeInfo.title;
-        author2 = item2.volumeInfo.authors;
-        publisher2 = item2.volumeInfo.publisher;
-        bookLink2 = item2.volumeInfo.previewLink;
-        bookIsbn2 = item2.volumeInfo.industryIdentifiers[1].identifier
-        bookImg2 = (item2.volumeInfo.imageLinks) ? item2.volumeInfo.imageLinks.thumbnail : placeHldr ;
-
-        // in production code, item.text should have the HTML entities escaped.
-        outputList.innerHTML += '<div class="row mt-4">' +
-                                formatOutput(bookImg1, title1, author1, publisher1, bookLink1, bookIsbn) +
-                                formatOutput(bookImg2, title2, author2, publisher2, bookLink2, bookIsbn2) +
-                                '</div>';
-
-        console.log(outputList);
-      }
-   }
-
-   /*
-   * card element formatter using es6 backticks and templates (indivial card)
-   * @param bookImg title author publisher bookLink
-   * @return htmlCard
-   */
-   function formatOutput(bookImg, title, author, publisher, bookLink, bookIsbn) {
-     // console.log(title + ""+ author +" "+ publisher +" "+ bookLink+" "+ bookImg)
-     var viewUrl = 'book.html?isbn='+bookIsbn; //constructing link for bookviewer
-     var htmlCard = `<div class="col-lg-6">
-       <div class="card" style="">
-         <div class="row no-gutters">
-           <div class="col-md-4">
-             <img src="${bookImg}" class="card-img" alt="...">
-           </div>
-           <div class="col-md-8">
-             <div class="card-body">
-               <h5 class="card-title">${title}</h5>
-               <p class="card-text">Author: ${author}</p>
-               <p class="card-text">Publisher: ${publisher}</p>
-               <a target="_blank" href="${viewUrl}" class="btn btn-secondary">Read Book</a>
-             </div>
-           </div>
-         </div>
-       </div>
-     </div>`
-     return htmlCard;
-   }
-
-   //handling error for empty search box
-   function displayError() {
-     alert("search term can not be empty!")
-   }
-
-
-});
+        stopBtn.addEventListener('click', stopSpeech);         
 
 
 
-
-
-
+        //             POSTS LOGIC CONTAINER ->
